d9acfb9f85fc8fbb8ec4d285751d7a0fd7d85f10
Eric Bidelman <ebidel@gmail.com>
2016-11-20 15:30:10 -0800

    Initial commit

---
+++ sw.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const PRECACHE = 'precache-v1.1';
+const RUNTIME = 'runtime-v1.1';
+
+const PRECACHE_URLS = [
+  '/',
+  'index.html', // Alias for /
+  'pwa-lighthouse.png',
+  'looped.mp3',
+];
+
+self.addEventListener('install', e => {
+  const cachingDone = caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(() => self.skipWaiting())
+  e.waitUntil(cachingDone);
+});
+
+self.addEventListener('activate',  e => {
+  const currentCaches = [PRECACHE, RUNTIME];
+
+  const cachesRemoved =  caches.keys().then(cacheNames => {
+    return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+  }).then(cachesToDelete => {
+    return Promise.all(cachesToDelete.map(cacheToDelete => {
+      return caches.delete(cacheToDelete);
+    }));
+  }).then(() => self.clients.claim());
+
+  e.waitUntil(cachesRemoved);
+});
+
+self.addEventListener('fetch', e => {
+  const requestURL = new URL(e.request.url);
+
+  // Skip cross-origin requests.
+  if (requestURL.origin !== self.location.origin) {
+    return;
+  }
+
+  const fetchingAndCaching = caches.match(e.request).then(cacheResp => {
+    return cacheResp || fetch(e.request).then(fetchResp => {
+      // Put a copy of the response in the runtime cache.
+      return caches.open(RUNTIME)
+        .then(cache => cache.put(e.request, fetchResp.clone()))
+        .then(() => fetchResp);
+    });
+  });
+
+  e.respondWith(fetchingAndCaching);
+});
